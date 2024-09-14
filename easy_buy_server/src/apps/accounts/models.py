7c1bd83074a57eebbc45a65afa16e13b1791735a
AkhlaqAltaf <akhlaqaltaf4@gmail.com>
2024-09-14 19:55:43 +0500

    REQUIREMENTS ISSUE

--- easy_buy_server/src/apps/accounts/models.py
+++ easy_buy_server/src/apps/accounts/models.py
@@ -1,19 +1,34 @@
-import string
-import uuid
-import  random
+# accounts/models.py
 
+import random
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
+
+
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
+
+        # Delete existing unverified user with the same email
+        try:
+            existing_user = self.get(email=email)
+            if not existing_user.is_verified:
+                existing_user.delete()
+            if existing_user.is_verified:
+                return {'user':existing_user,'status_code':600,'message':"Email Already Registered"}
+
+
+        except CustomUser.DoesNotExist:
+            pass
+
         user = self.model(email=email, **extra_fields)
         user.verification_code = self.generate_verification_code()
         user.set_password(password)
         user.save(using=self._db)
-        return user
+        return  {'user':user,'status_code':700,'message':"Successfully Registered"}
+
 
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
@@ -37,8 +52,9 @@ class CustomUserManager(BaseUserManager):
 
 
 class CustomUser(AbstractBaseUser):
+
     email = models.EmailField(unique=True)
-    verification_code = models.CharField(max_length=32, unique=True, blank=True, null=True)
+    verification_code = models.CharField(max_length=32, unique=False, blank=True, null=True)
     is_verified = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
@@ -52,16 +68,8 @@ class CustomUser(AbstractBaseUser):
     def __str__(self):
         return self.email
 
-
     def has_perm(self, perm, obj=None):
-        """
-        Does the user have a specific permission?
-        """
         return self.is_superuser
 
     def has_module_perms(self, app_label):
-        """
-        Does the user have permissions to view the app `app_label`?
-        """
         return self.is_superuser
-
